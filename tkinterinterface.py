419572800812df68ca973b4659538757514b13dd
teganasprey <82981420+teganasprey@users.noreply.github.com>
2023-03-05 12:39:50 -0500

    Update tkinterinterface.py

--- tkinterinterface.py
+++ tkinterinterface.py
@@ -1,13 +1,19 @@
 import tkinter as tk
+from tkinter import messagebox
+
+
+def button_pressed():
+    messagebox.showinfo("Message", "Hey There! I hope you are doing well.")
+
 
 window = tk.Tk()
 window.attributes('-fullscreen', True)
 greeting = tk.Button(
-    text="you better work",
-    font=("Arial", 25),
+    text="work!",
+    font=("Arial", 50),
     width=30,
-    height=30
-
+    height=15,
+    command = button_pressed
 )
 greeting.pack()
 window.mainloop()
